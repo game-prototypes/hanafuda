1ba8a456474403dbc78e8c07c73d81d39922a4b5
angrykoala <angrykoala@outlook.es>
2022-01-29 11:52:18 +0000

    wip mobile UI #20

--- autoload/scene_switcher.gd
+++ autoload/scene_switcher.gd
@@ -2,6 +2,7 @@ extends Node
 
 export(PackedScene) var game_round_scene:PackedScene
 export(PackedScene) var main_menu_scene:PackedScene
+export(PackedScene) var mobile_game_scene:PackedScene
 
 func _ready():
 	assert(game_round_scene!=null, "Game Scene not set")
@@ -9,7 +10,11 @@ func _ready():
 
 
 func switch_to_game_round_scene():
-	_switch_to_scene(game_round_scene)
+	var mobile=ProjectSettings.get_setting("application/config/mobile")
+	if mobile:
+		_switch_to_scene(mobile_game_scene)
+	else:
+		_switch_to_scene(game_round_scene)
 	
 func switch_to_main_menu_scene():
 	_switch_to_scene(main_menu_scene)
